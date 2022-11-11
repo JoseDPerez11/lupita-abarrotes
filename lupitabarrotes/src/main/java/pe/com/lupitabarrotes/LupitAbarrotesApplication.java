53d979b03b9f64a6728f640f158f51c1dd415342
JoseDPerez11 <danielpc334@gmail.com>
2022-11-11 13:32:23 -0500

    Subida inicial 11/11

---
+++ lupitabarrotes/src/main/java/pe/com/lupitabarrotes/LupitAbarrotesApplication.java
@@ -0,0 +1,13 @@
+package pe.com.lupitabarrotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LupitAbarrotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LupitAbarrotesApplication.class, args);
+	}
+
+}
---
+++ lupitabarrotes/src/test/java/pe/com/lupitabarrotes/LupitAbarrotesApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.com.lupitabarrotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LupitAbarrotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
